9269eb898425bcfa10003d548a3f2d90991f96f4
YunsenaMir <mirr26.aysena@mail.ru>
2021-11-24 17:27:23 +0300

    Some unit test code modifications have been added

--- src/test/java/CalculationTest.java
+++ src/test/java/CalculationTest.java
@@ -1,4 +1,5 @@
 import com.service.Calculation;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,50 +12,45 @@ public class CalculationTest {
     private final String expression = "120 ÷ 25%";
     private final Calculation calculation = new Calculation(expression, operations);
 
+    @BeforeEach
     private void setValues() {
         calculation.setParametersValues();
     }
 
+
     @Test
     public void checkValidateInput() {
-        setValues();
         assertTrue(calculation.isValidateInput());
     }
 
     @Test
     public void onlyOneOperandIsEntered() {
-        setValues();
         assertFalse(calculation.onlyOneOperandIsEntered());
     }
 
 
     @Test
     public void countChar() {
-        setValues();
         assertEquals(1, calculation.countChar(expression, "%"));
     }
 
     @Test
     public void calculate() {
-        setValues();
         assertEquals("4", calculation.calculate());
     }
 
     @Test
     public void calculatePercent() {
-        setValues();
         assertEquals(30, calculation.calculatePercent(false));
     }
 
     @Test
     public void substringNumberFromPercent() {
-        setValues();
         assertEquals(25.0f, calculation.substringNumberFromPercent("25%"));
     }
 
     @Test
     public void setIntegerFormatIfNeeded() {
-        setValues();
         assertEquals("25", calculation.setIntegerFormatIfNeeded(25.00f));
     }
 }
